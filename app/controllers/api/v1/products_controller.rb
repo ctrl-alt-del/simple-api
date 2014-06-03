4050024850da4a316676ad4d8a9c39c50b9c2e89
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-03 00:10:14 -0700

    add fetch_product method;

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,10 @@
 require 'application_controller'
 
 class Api::V1::ProductsController < ApplicationController
+	def fetch_product
+		@product = Product.find_by_id(params[:id])
+	end
+
 	def index
 		@products = Product.all
     	# render :json => @products
